a5b1194e7b7f5883076912b575647c3da86bbabb
TUXEDO FALCON <alerod0204@gmail.com>
2020-09-14 20:36:55 +0000

    Dog :)

--- index.java
+++ index.java
@@ -8,10 +8,47 @@ void draw() {
   ellipse(mouseX, mouseY, 1, 1);
   println(mouseX, mouseY);
   rectMode(CENTER);
- 
-  //Make a creature!!! 
-  //all of your programming should be after this 👍
+
+//Body
+
+  rect(200, 200, 120, 50);
   
+  
+  // Ears
+
+  rect(250, 180, 20, 30);
+
+  
+  //Nose
+
+  rect( 275, 190, 30, 20);
+  ellipse(293, 185, 5, 5)
+
+
+ //Tail
+
+ rect(120, 200, 40, 10);
+  
+
+  //Paws
+
+  rect(150, 235, 10, 20);
+  rect(180, 235, 10, 20);
+  rect(210, 235, 10, 20);
+  rect(240, 235, 10, 20);
+
+
+  //Spots
+
+  ellipse(170, 190, 20, 20);
+  ellipse(230, 210, 15, 15);
+  ellipse(155, 215, 15, 15);
+
+
+
+  
+
+
  
   
   
